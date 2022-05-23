b62ba2d6507d6e36a58054a9b8a169725c211f3f
Brian Hoang <brian.hd.hoang@codurance.com>
2022-05-23 17:49:05 +0100

    green

---
+++ src/main/java/Bag.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Bag {
+    public final Integer sizeLimit;
+    private final ArrayList<Item> contents = new ArrayList<>();
+
+    public Bag(Integer sizeLimit) {
+        this.sizeLimit = sizeLimit;
+    }
+
+    public Integer size() {
+        return contents.size();
+    }
+
+    public String getContentsOfBag() {
+        if (contents.isEmpty()) return "empty\n";
+
+        var string = new StringBuilder();
+
+        for (int i = 0; i < contents.size(); i++) {
+            String itemName = contents.get(i).name();
+            string.append(itemName);
+
+            var notLastItem = i != contents.size() - 1;
+            if (notLastItem) {
+                string.append(", ");
+            }
+        }
+        string.append("\n");
+        return string.toString();
+    }
+
+    public void sort() {
+        contents.sort((item1, item2) -> item1.name().compareToIgnoreCase(item2.name()));
+    }
+
+    public void add(Item item) {
+        contents.add(item);
+    }
+
+    public Item moveLastItem() {
+        var lastItem = contents.get(contents.size() - 1);
+        contents.remove(lastItem);
+        return lastItem;
+    }
+}
--- src/main/java/Durance.java
+++ src/main/java/Durance.java
@@ -1,57 +1,34 @@
-import java.util.ArrayList;
-
 public class Durance {
-    private final ArrayList<Item> backpack = new ArrayList<Item>();
-    private ArrayList<Item> bag1 = new ArrayList<Item>();
-    private ArrayList<Item> bag2 = new ArrayList<Item>();
-    private ArrayList<Item> bag3 = new ArrayList<Item>();
-    private ArrayList<Item> bag4 = new ArrayList<Item>();
+    private final Bag backpack = new Bag(8);
+    private Bag bag1 = new Bag(4);
+    private Bag bag2 = new Bag(4);
+    private Bag bag3 = new Bag(4);
+    private Bag bag4 = new Bag(4);
 
     public void addItem(Item item) {
         backpack.add(item);
     }
 
     public String displayBagContents() {
-        var backpackContents = "Backpack: " + getContentsOfBag(backpack);
-        var bag1Contents = "Bag with no category: " + getContentsOfBag(bag1);
-        var bag2Contents = "Bag with no category: " + getContentsOfBag(bag2);
-        var bag3Contents = "Bag with no category: " + getContentsOfBag(bag3);
-        var bag4Contents = "Bag with no category: " + getContentsOfBag(bag4);
+        var backpackContents = "Backpack: " + backpack.getContentsOfBag();
+        var bag1Contents = "Bag with no category: " + bag1.getContentsOfBag();
+        var bag2Contents = "Bag with no category: " + bag2.getContentsOfBag();
+        var bag3Contents = "Bag with no category: " + bag3.getContentsOfBag();
+        var bag4Contents = "Bag with no category: " + bag4.getContentsOfBag();
 
         return backpackContents + bag1Contents + bag2Contents + bag3Contents + bag4Contents;
     }
 
-    private String getContentsOfBag(ArrayList<Item> bag) {
-        if (bag.isEmpty()) return "empty\n";
-
-        var string = new StringBuilder();
-
-        for (int i = 0; i < bag.size(); i++) {
-            String itemName = bag.get(i).name();
-            string.append(itemName);
-
-            var notLastItem = i != bag.size() - 1;
-            if (notLastItem) {
-                string.append(", ");
-            }
-        }
-        string.append("\n");
-        return string.toString();
-    }
-
     public void organiseBags() {
-        moveContents(backpack, bag1, 8);
-        moveContents(bag1, bag2, 4);
+        moveContents(backpack, bag1);
+        moveContents(bag1, bag2);
 
-        backpack.sort((item1, item2) -> item1.name().compareToIgnoreCase(item2.name()));
+        backpack.sort();
     }
 
-    private void moveContents(ArrayList<Item> firstBag, ArrayList<Item> secondBag, Integer limit) {
-        while (firstBag.size() > limit) {
-            var lastIndex = firstBag.size() - 1;
-
-            secondBag.add(firstBag.get(lastIndex));
-            firstBag.remove(lastIndex);
+    private void moveContents(Bag firstBag, Bag secondBag) {
+        while (firstBag.size() > firstBag.sizeLimit) {
+            secondBag.add(firstBag.moveLastItem());
         }
     }
 
