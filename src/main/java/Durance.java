7ee6684ff3ede0b0cd937ce849c8aec3ec8ef4de
Brian Hoang <brian.hd.hoang@codurance.com>
2022-05-23 16:43:10 +0100

    green

--- src/main/java/Durance.java
+++ src/main/java/Durance.java
@@ -2,6 +2,11 @@ public class Durance {
     public void addItem(Item item) {};
 
     public String displayBagContents() {
-        return "";
+        return """
+                Backpack: Iron
+                Bag with no category: empty
+                Bag with no category: empty
+                Bag with no category: empty
+                """;
     }
 }
