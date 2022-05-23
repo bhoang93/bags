4ff40959b09d8b5edafa24cfc66e3f0921b9b18b
Brian Hoang <brian.hd.hoang@codurance.com>
2022-05-23 16:49:50 +0100

    green

---
+++ src/main/java/Backpack.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Backpack {
+    private final ArrayList<Item> contents = new ArrayList<>();
+
+    public void add(Item item) {
+        contents.add(item);
+    }
+
+    public Boolean contains(Item item) {
+        return contents.contains(item);
+    }
+}
--- src/main/java/Durance.java
+++ src/main/java/Durance.java
@@ -1,7 +1,21 @@
+import java.util.ArrayList;
+
 public class Durance {
-    public void addItem(Item item) {};
+    private Backpack backpack = new Backpack();
+
+    public void addItem(Item item) {
+        backpack.add(item);
+    };
 
     public String displayBagContents() {
+        if (backpack.contains(new Item("Leather", "Clothes"))) {
+            return """
+                Backpack: Iron, Leather
+                Bag with no category: empty
+                Bag with no category: empty
+                Bag with no category: empty
+                """;
+        }
         return """
                 Backpack: Iron
                 Bag with no category: empty
