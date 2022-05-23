b1c4292ab776384abb8c013b05a84996452c398e
Brian Hoang <brian.hd.hoang@codurance.com>
2022-05-23 16:45:19 +0100

    green

--- src/test/java/BagsShould.java
+++ src/test/java/BagsShould.java
@@ -17,4 +17,20 @@ public class BagsShould {
 
         assertEquals(expected, result);
     }
+
+    @Test
+    void add_iron_and_leather_to_the_backpack() {
+        Durance durance = new Durance();
+        durance.addItem(new Item("Iron", "Metal"));
+        durance.addItem(new Item("Leather", "Clothes"));
+        String result = durance.displayBagContents();
+        String expected = """
+                Backpack: Iron, Leather
+                Bag with no category: empty
+                Bag with no category: empty
+                Bag with no category: empty
+                """;
+
+        assertEquals(expected, result);
+    }
 }
